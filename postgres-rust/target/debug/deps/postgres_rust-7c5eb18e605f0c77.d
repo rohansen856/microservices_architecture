7081288e12bbb5428afed8866d00ec33b0c6ee88
rohansen856 <rohansen856@gmail.com>
2024-12-28 03:12:10 +0530

    new: postgres database integration in rust added

---
+++ postgres-rust/target/debug/deps/postgres_rust-7c5eb18e605f0c77.d
@@ -0,0 +1,5 @@
+/home/rcsen/Documents/rust/microservices_architecture/postgres-rust/target/debug/deps/libpostgres_rust-7c5eb18e605f0c77.rmeta: src/main.rs
+
+/home/rcsen/Documents/rust/microservices_architecture/postgres-rust/target/debug/deps/postgres_rust-7c5eb18e605f0c77.d: src/main.rs
+
+src/main.rs:
---
+++ postgres-rust/target/debug/deps/postgres_rust-a12141419debd4ae.d
@@ -0,0 +1,5 @@
+/home/rcsen/Documents/rust/microservices_architecture/postgres-rust/target/debug/deps/libpostgres_rust-a12141419debd4ae.rmeta: src/main.rs
+
+/home/rcsen/Documents/rust/microservices_architecture/postgres-rust/target/debug/deps/postgres_rust-a12141419debd4ae.d: src/main.rs
+
+src/main.rs:
